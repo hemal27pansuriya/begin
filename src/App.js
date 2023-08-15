9cf7debea5c6fa380358ca285cfbb93f97f54ef7
yudiz-hemal-pansuriya <hemal.pansuriya@yudiz.com>
2023-08-15 13:44:33 +0530

    Add: React router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light')
@@ -24,21 +29,31 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#042743'
       showAlert('Dark mode has been enabled!', 'Success')
+      document.title = 'Text Utils - Dark Mode'
     } else {
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert('Light mode has been enabled!', 'Success')
+      document.title = 'Text Utils'
     }
   }
 
   return (
     <>
-      <Navbar title='TextUtils' mode={mode} changeMode={handleMode} />
-      <Alert alert={alert}/>
-      <div className='container my-3'>
-        {/* <About /> */}
-        <TextForm heading='Enter Your Text Below' mode={mode} showAlert={showAlert}/>
-      </div>
+      <Router>
+        <Navbar title='TextUtils' mode={mode} changeMode={handleMode} />
+        <Alert alert={alert} />
+        <div className='container my-3'>
+          <Switch>
+            <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/" element={
+              <TextForm heading='Enter Your Text Below' mode={mode} showAlert={showAlert} />
+            }>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
